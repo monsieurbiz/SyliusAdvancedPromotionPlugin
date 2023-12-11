5005e8bf478b96ada3e6196fddfce9c8cf14c111
Maxime Huran <m.huran@monsieurbiz.com>
2023-12-11 11:44:39 +0100

    Init plugin

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# SyliusAdvancedPromotionPlugin
-Allows you promotions after tax and multi coupon
+<p align="center">
+    <a href="https://monsieurbiz.com" target="_blank">
+        <img src="https://monsieurbiz.com/logo.png" width="250px" alt="Monsieur Biz logo" />
+    </a>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://monsieurbiz.com/agence-web-experte-sylius" target="_blank">
+        <img src="https://demo.sylius.com/assets/shop/img/logo.png" width="200px" alt="Sylius logo" />
+    </a>
+    <br/>
+    <img src="https://monsieurbiz.com/assets/images/sylius_badge_extension-artisan.png" width="100" alt="Monsieur Biz is a Sylius Extension Artisan partner">
+</p>
+
+<h1 align="center">Advanced Promotion for Sylius</h1>
+
+[![Advanced Promotion Plugin license](https://img.shields.io/github/license/monsieurbiz/SyliusAdvancedPromotionPlugin?public&nocache)](https://github.com/monsieurbiz/SyliusAdvancedPromotionPlugin/blob/master/LICENSE)
+[![Tests Status](https://github.com/monsieurbiz/SyliusAdvancedPromotionPlugin/actions/workflows/tests.yaml/badge.svg?branch=master&event=push)](https://github.com/monsieurbiz/SyliusAdvancedPromotionPlugin/actions?query=workflow%3ATests)
+[![Security Status](https://github.com/monsieurbiz/SyliusAdvancedPromotionPlugin/actions/workflows/security.yaml/badge.svg?branch=master&event=push)](https://github.com/monsieurbiz/SyliusAdvancedPromotionPlugin/actions?query=workflow%3ASecurity)
+
+This plugins add features on promotions.
+You can define if a promotion need to be applied before or after tax.
+You can use multiple coupons in your orders.
+
+# Installation
+
+```bash
+composer require monsieurbiz/sylius-advanced-promotion-plugin
+```
+
+# Configuration 
+
+In progress
---
+++ src/DependencyInjection/MonsieurBizSyliusAdvancedPromotionExtension.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Advanced Promotion plugin for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAdvancedPromotionPlugin\DependencyInjection;
+
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class MonsieurBizSyliusAdvancedPromotionExtension extends Extension implements PrependExtensionInterface
+{
+    use PrependDoctrineMigrationsTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public function load(array $config, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAlias(): string
+    {
+        return 'monsieurbiz_advanced_promotion';
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDoctrineMigrations($container);
+    }
+
+    protected function getMigrationsNamespace(): string
+    {
+        return 'MonsieurBiz\SyliusAdvancedPromotionPlugin\Migrations';
+    }
+
+    protected function getMigrationsDirectory(): string
+    {
+        return '@MonsieurBizSyliusAdvancedPromotionPlugin/Migrations';
+    }
+
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return [
+            'Sylius\Bundle\CoreBundle\Migrations',
+        ];
+    }
+}
